2afd81972d268b68189790d6a6ec146d5cbf59e1
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2015-02-27 14:01:55 +0800

    logging setup. closes #29

--- src/neumann/__init__.py
+++ src/neumann/__init__.py
@@ -1,1 +1,10 @@
 __author__ = 'guilherme'
+
+
+from neumann.utils import config
+from neumann.utils.logger import Logger
+
+conf = config.load_configuration()
+
+if conf:
+    Logger.setup_logging(conf["log_config_file"])
