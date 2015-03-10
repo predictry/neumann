9a549b4588fe2c09813e6d7d94f7c635279b3c26
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2015-03-10 18:00:57 +0800

    Moved tenant to entities module. Closes #49

--- src/neumann/core/entities/tenant.py
+++ src/neumann/core/entities/tenant.py
@@ -17,117 +17,9 @@ from neumann.utils import config
 from neumann.utils.logger import Logger
 
 
-class TenantRecommendationSetting(object):
-
-    def __init__(self, tenant, method):
-        self.tenant = tenant
-        self.method = method
-
-
-def get_db_connection_string():
-    """
-
-    :return:
-    """
-
-    #todo: configuration should be a class, not a dictionary
-    #todo: build unit test
-    configuration = config.load_configuration()
-
-    if "client-db" in configuration:
-
-        try:
-
-            db_conf = configuration["client-db"]
-
-            s = "{DIALECT}{DRIVER}://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}".format(
-                DIALECT=db_conf["dialect"],
-                DRIVER=''.join(["+", db_conf["driver"]]) if db_conf["driver"] else "",
-                USERNAME=db_conf["username"],
-                PASSWORD=db_conf["password"],
-                HOST=db_conf["host"],
-                DATABASE=db_conf["database"])
-
-        except KeyError as err:
-
-            Logger.error("Error reading Client DB configuration:\n\t{0}".format(err))
-        else:
-
-            return s
-
-    else:
-
-        raise KeyError("Client DB configuration is missing from configuration file. `{0}` key was not found".format(
-            "client-db"
-        ))
-
-
-def get_tenants():
-    """
-
-    :return:
-    """
-
-    engine = rdb.get_engine(connection_string=get_db_connection_string())
-
-    try:
-        conn = engine.connect()
-
-    except Exception as err:
-
-        Logger.error(err)
-
-        raise err
-
-    else:
-
-        s = sqlalchemy.sql.select([store.Tenant])
-
-        result = conn.execute(s)
-
-        tenants = []
-
-        for row in result:
-
-            tenant = store.Tenant()
-            tenant.id = row["id"]
-            tenant.name = row["name"]
-
-            tenants.append(tenant)
-
-        return tenants
-
-
-def get_tenant_widgets(tenant_id):
-    """
-
-    :return:
-    """
-
-    engine = rdb.get_engine(connection_string=get_db_connection_string())
-    conn = engine.connect()
-
-    s = sqlalchemy.sql.select([store.Widget]).where(store.Widget.site_id == tenant_id)
-
-    result = conn.execute(s)
-
-    widgets = []
-
-    for row in result:
-
-        widget = store.Widget()
-        widget.id = row["id"]
-        widget.site_id = row["site_id"]
-        widget.reco_type = row["reco_type"]
-
-        widgets.append(widget)
-
-    return widgets
-
-
 @neo4j.CypherQuery("MATCH (n :`{LABEL}`) RETURN DISTINCT LABELS(n) AS labels".format(
     LABEL=store.LABEL_ITEM))
-def get_active_tenants_names(**kwargs):
+def get_active_tenants(**kwargs):
     """
 
     :param kwargs:
@@ -142,20 +34,9 @@ def get_active_tenants_names(**kwargs):
     return tenants
 
 
-@neo4j.CypherQuery("MATCH (n :`{LABEL}`) RETURN COUNT (n) AS n".format(LABEL=store.LABEL_ITEM))
-def item_count(**kwargs):
-    """
-
-    :param kwargs:
-    :return:
-    """
-
-    return kwargs["result"][0]["n"]
-
-
 @neo4j.CypherQuery("MATCH (n :`{LABEL}`) WHERE {{tenant}} IN LABELS(n) RETURN COUNT (n) AS n".format(
     LABEL=store.LABEL_ITEM))
-def item_count_for_tenant(tenant, **kwargs):
+def get_item_count_for_tenant(tenant, **kwargs):
     """
 
     :param tenant:
@@ -167,7 +48,7 @@ def item_count_for_tenant(tenant, **kwargs):
 
 
 @neo4j.CypherQuery("MATCH (n :`{LABEL}` {{id: {{id}} }}) RETURN n".format(LABEL=store.LABEL_ITEM))
-def get_item(id, **kwargs):
+def get_item_node(id, **kwargs):
     """
 
     :param id:
@@ -178,37 +59,28 @@ def get_item(id, **kwargs):
     return kwargs["result"][0]["n"]
 
 
-def get_tenant_list_of_items_id(tenant):
-
-    n = item_count_for_tenant(tenant=tenant)
-
-    limit = 50000
-    skip = 0
+#todo: refactor: retrieve property of entity
+def get_tenant_list_of_items_id(tenant, skip=0, limit=10):
 
     q = "MATCH (n :`{LABEL}` :`{TENANT}`) RETURN n.id AS id SKIP {{skip}} LIMIT {{limit}}".format(
         LABEL=store.LABEL_ITEM, TENANT=tenant
     )
 
-    items = list()
+    params = [neo4j.Parameter("limit", limit), neo4j.Parameter("skip", skip)]
 
-    while n > skip:
+    query = neo4j.Query(q, params)
 
-        params = [neo4j.Parameter("limit", limit), neo4j.Parameter("skip", skip)]
+    r = neo4j.run_query(query)
 
-        query = neo4j.Query(q, params)
-
-        r = neo4j.run_query(query, commit=False)
-
-        items.extend([x["id"] for x in r])
-
-        skip += limit
+    items = [x["id"] for x in r]
 
     return items
 
 
 def download_tenant_items_to_a_folder(tenant):
 
-    n = item_count_for_tenant(tenant=tenant)
+    #TODO: implement paging (tenant, limit, skip)
+    n = get_item_count_for_tenant(tenant=tenant)
 
     limit = 30000
     skip = 0
